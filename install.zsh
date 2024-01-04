df51bc778a38333ec3ee741c146e3b7c0cfcf899
Luca Campese <me@lcampese.net>
2024-01-04 20:02:49 +0100

    Add install script

---
+++ install.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+source ~/.zshrc
+touch ~/.hushlogin
+
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim ~/.vim
+vim +qall
